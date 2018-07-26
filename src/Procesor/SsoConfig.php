6f2fc256bad8a33b9974a4f962831ea5994b2c2d
RazzTek <garth.mikesell@gmail.com>
2018-07-26 12:58:06 -0600

    remove static info

--- src/Procesor/SsoConfig.php
+++ src/Procesor/SsoConfig.php
@@ -1,9 +1,9 @@
 <?php
 /* CAS Protocol Configuration */
-$cas_host = 'login.usu.edu';
+$cas_host = 'host name';
 $cas_port = 443;
 $cas_context = '/cas';
-$cas_server_ca_cert_path = '/var/www/letmein/login.usu.edu.pem';
+$cas_server_ca_cert_path = 'path to cert';
 
 
 
